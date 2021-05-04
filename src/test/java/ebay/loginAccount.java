b9b2cb4e292cea95ec8a762650395cad593251dc
MuhammadAli611 <chma616@gmail.com>
2021-05-04 13:32:24 -0400

    ebay

--- src/test/java/ebay/loginAccount.java
+++ src/test/java/ebay/loginAccount.java
@@ -15,14 +15,14 @@ public class loginAccount {
         driver.get("https://www.google.com/");
           driver.get("https://signup.ebay.com");
           driver.findElement(By.id("gh-ug")).click();
-           driver.findElement(By.id("create-account-link")).click();
+          driver.findElement(By.id("create-account-link")).click();
            driver.findElement(By.id("firstname")).sendKeys("Muhammad");
            driver.findElement(By.id("lastname")).sendKeys("Ali");
            driver.findElement(By.id("Email")).sendKeys("chma616@gmail.com");
            driver.findElement(By.id("password")).sendKeys("Alidhoom3756");
            driver.findElement(By.id("EMAIL_REG_FORM_SUBMIT")).click();
-         //driver.findElement(By.id("gh-logo")).click();
-        driver.get("https://www.ebay.com/");
+         driver.findElement(By.id("gh-logo")).click();
+       driver.get("https://www.ebay.com/");
         driver.findElement(By.id("gh-shop-a")).click();
         driver.findElement(By.id("gh-p-3")).click();
         driver.findElement(By.id("popular_solution_1")).click();
